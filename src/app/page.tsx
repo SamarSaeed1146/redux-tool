eab7d07200cebe28c45525678972ef810e1c5b0f
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-08-15 01:01:56 +0500

    Hook In Redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  const data = [{name: "Muhammad Samar Saeed", age: 18}];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Redux Tool</h1>
-      {data[0].name}
     </main>
   );
 }
