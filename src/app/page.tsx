5cb0e032e01f3188428d0d46f8b0b413e7c6fcd8
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-08-16 14:17:08 +0500

    Crud Operation In Redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-
+"use client";
 import CartList from "./components/CartList";
 import ProductList from "./components/ProductList";
-import { useGetPostsQuery } from "./store/slices/postApi";
+import {
+  useGetPostsByIdQuery,
+  useGetPostsQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} from "./store/slices/postApi";
 
 export default function Home() {
-  const { data } = useGetPostsQuery("");
+  // const { data } = useGetPostsQuery("");
+  // const { data } = useGetPostsByIdQuery(2);
+  // const [updatepost, result] = useCreatePostMutation();
+  // const [updatepost, result] = useUpdatePostMutation();
+  const [updatepost, result] = useDeletePostMutation();
+  console.log(result);
   return (
     <main className="p-6">
       <h1 className="text-center">Redux Toolkit RTK Query</h1>
+      <br />
+      <button onClick={() => updatepost(111)}>
+        delete user
+      </button>
       {/* <ProductList/>
       <h1 className="my-5">Cart List</h1>
       <CartList /> */}
--- src/app/store/slices/postApi.tsx
+++ src/app/store/slices/postApi.tsx
@@ -1,14 +1,40 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const postApi = createApi({
-    reducerPath: 'postApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
-    endpoints: (builder) => ({
-        getPosts: builder.query({
-            query: () => '/users',
-        }),
+  reducerPath: "postApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => `/users`,
     }),
-})
+    getPostsById: builder.query({
+      query: (id) => `/users/${id}`,
+    }),
+    createPost: builder.mutation({
+      query: (data) => ({
+        url: `/users`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updatePost: builder.mutation({
+      query: (data) => ({
+        url: `/users/${data.id}`,
+        method: "PATCH",
+        body: data,
+      }),
+    }),
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
 
-export const { useGetPostsQuery } = postApi
+export const { useGetPostsQuery, useGetPostsByIdQuery, useCreatePostMutation, useUpdatePostMutation, useDeletePostMutation } =
+  postApi;
